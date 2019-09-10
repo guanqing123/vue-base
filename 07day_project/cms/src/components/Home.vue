0bbf4e80142c466ace1e046a18c08dda61d3b492
gq <guanqinghz@163.com>
2019-09-10 21:47:36 +0800

    初始化九宫格

--- 07day_project/cms/src/components/Home.vue
+++ 07day_project/cms/src/components/Home.vue
@@ -1,10 +1,71 @@
 <template>
   <div>
+    <!-- 上有轮播 -->
     <mt-swipe :auto="4000">
-      <mt-swipe-item>1</mt-swipe-item>
-      <mt-swipe-item>2</mt-swipe-item>
-      <mt-swipe-item>3</mt-swipe-item>
+      <mt-swipe-item v-for="img in imgs" :key="img">
+        <img :src="img" />
+      </mt-swipe-item>
     </mt-swipe>
+
+    <!-- 下有九宫格 -->
+    <div class="grid">
+      <ul>
+        <li>
+        <a href="">
+          <span class="back-img"></span>
+          <div>1111</div>
+        </a>
+      </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>2222</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>3333</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>4444</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>5555</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>6666</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>7777</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>8888</div>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <span class="back-img"></span>
+            <div>9999</div>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -13,13 +74,50 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      imgs: [] // 轮播图数据
     }
+  },
+  created () {
+    this.$axios.get('/homepage/getHomePageList')
+      .then(res => {
+        this.imgs = res.data.data.map(item => item.fileRealPath)
+        console.log(this.imgs)
+      })
+      .catch(err => console.log(err))
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.mint-swipe {
+  width: 100%;
+  height: 200px;
+}
+.mint-swipe img {
+  width: 100%;
+  height: 100%;
+}
+/*九宫格样式*/
+.grid span{
+  display: inline-block;
+  margin: 10px 0;
+  width: 50px;
+  height: 50px;
+  background-repeat: round;
+}
+.back-img{
+  background-image: url("../../static/img/callme.png");
+}
+ul{
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+li{
+  list-style: none;
+  float: left;
+  width: 33.333%;
+  text-align: center;
+}
 </style>
--- 07day_project/cms/src/main.js
+++ 07day_project/cms/src/main.js
@@ -6,14 +6,24 @@ import router from './router'
 
 // 引入 全局样式
 import '../static/css/global.css'
+
+// axios 开始
+import Axios from 'axios'
+// axios 结束
+
+// mint-ui 开始
 // 引入css
 import 'mint-ui/lib/style.css'
 // 引入Mint-ui
 import MintUI from 'mint-ui'
 // 安装插件
 Vue.use(MintUI) // 里面其实做的就是注册所有的全局组件,和给Vue.prototype挂载一些对象,方便你使用  组件内的this.xxx就能用了
+// mint-ui 结束
 
 Vue.config.productionTip = false
+// 给Vue的原型挂载 $axios 属性
+Vue.prototype.$axios = Axios
+Axios.defaults.baseURL = 'https://wx.hongyancloud.com/wxDev'
 
 /* eslint-disable no-new */
 new Vue({
